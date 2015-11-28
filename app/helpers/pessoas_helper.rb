7bfcebd8c8e8a21e82a6d3c0b6e511b6c21851f5
Priscila Caproni <pcaproni@gmail.com>
2015-11-28 18:09:34 -0200

    Parte 6

--- app/helpers/pessoas_helper.rb
+++ app/helpers/pessoas_helper.rb
@@ -1,6 +1,5 @@
 module PessoasHelper
 	def calculo_imc(pessoa)
-		imc = pessoa.peso/(pessoa.altura**2)
-		imc.round(1)
+		imc = (pessoa.peso/(pessoa.altura**2)).round(1) unless pessoa.peso.nil? || pessoa.altura.nil?
 	end
 end
